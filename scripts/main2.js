873a0c303163f0873c4db2ca745196a5b4d429e9
Jaemin Jin <jaemin_95@csu.fullerton.edu>
2021-06-17 16:17:24 -0700

    deleted

--- scripts/main2.js
+++
@@ -1,9 +0,0 @@
-// These scripts are part of 'comment.html'
-(function (window) {
-  'use strict';
-  var App = window.App;
-  var Truck = App.Truck;
-  var DataStore = App.DataStore;
-  var myTruck = new Truck('ncc-1701', new DataStore());
-  window.myTruck = myTruck;
-})(window);
